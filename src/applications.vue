ac95df948b6fe692963394456fe0000576a60480
Humiao <1124003166@qq.com>
2020-09-21 18:33:13 +0800

    Vue-demo项目初始化

---
+++ src/applications.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>HelloWorld</h1>
+</template>
+
+<script>
+
+  export default {
+    name: "Applications",
+    components: {},
+    data() {
+      return {};
+    },
+    methods: {},
+  };
+</script>
+
+<style scoped lang="stylus">
+</style>
---
+++ src/modules/module-a/components/component-a.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+  export default {
+    name: 'ComponentA',
+  }
+</script>
+
+<style scoped>
+
+</style>
