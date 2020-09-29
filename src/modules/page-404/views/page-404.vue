bf9ef26061e4809657533f71615f954d83419c6e
Humiao <1124003166@qq.com>
2020-09-29 16:35:44 +0800

    新增404页面跳转

--- src/modules/page-404/views/page-404.vue
+++ src/modules/page-404/views/page-404.vue
@@ -1,7 +1,12 @@
 <template>
   <hips-view class="page-404" :header-height="50" :footer-height="48" header-fixed content-frozen>
     <!--导航栏-->
-    <hips-nav-bar :back-button="{showLeftArrow: true}" @nav-bar-back-click="goBack()"/>
+    <hips-nav-bar :back-button="{showLeftArrow: true}" title="404" @nav-bar-back-click="goBack()"/>
+    <div id="clock">
+      <p class="date">...404 error page...</p>
+      <p class="time">{{ time }}</p>
+      <p class="text">{{ date }}</p>
+    </div>
   </hips-view>
 </template>
 
@@ -16,7 +21,8 @@
     },
     data() {
       return {
-        tabsActive: 0
+        time: '',
+        date: ''
       }
     },
     methods: {
@@ -38,5 +44,47 @@
     .hips-nav-bar__title {
       color #fff;
     }
+  }
+
+  .page-404 {
+    background: #0f3854;
+    background: -webkit-radial-gradient(center ellipse, #0a2e38 0%, #000000 70%);
+    background: radial-gradient(ellipse at center, #0a2e38 0%, #000000 70%);
+    background-size: 100%;
+  }
+
+  p {
+    margin: 0;
+    padding: 0;
+  }
+
+  #clock {
+    font-family: 'Share Tech Mono', monospace;
+    color: #ffffff;
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    color: #daf6ff;
+    text-shadow: 0 0 20px #0aafe6, 0 0 20px rgba(10, 175, 230, 0);
+  }
+
+  #clock .time {
+    letter-spacing: 0.05em;
+    font-size: 60px;
+    padding: 5px 0;
+  }
+
+  #clock .date {
+    letter-spacing: 0.1em;
+    font-size: 15px;
+  }
+
+  #clock .text {
+    letter-spacing: 0.1em;
+    font-size: 12px;
+    padding: 20px 0 0;
   }
 </style>
