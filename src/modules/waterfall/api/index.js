ff6f25a51b7ce15aa0c7323973eac9f551b006dd
Humiao <1124003166@qq.com>
2020-10-12 11:36:12 +0800

    新增瀑布流界面内容

---
+++ src/modules/waterfall/api/index.js
@@ -0,0 +1,22 @@
+const waterfallService = {
+  getImageList: () => {
+    return [
+      'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1138436920,1957889194&fm=26&gp=0.jpg',
+      'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1464383660,1544676293&fm=26&gp=0.jpg',
+      'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=997668,2649764278&fm=26&gp=0.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602479258213&di=8785403bb611212c76207648f96cf523&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201902%2F09%2F20190209172132_dqmyd.thumb.400_0.png',
+      'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3135328207,3052731152&fm=26&gp=0.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602479246741&di=bad391bf3f7b64971491574346e394d1&imgtype=0&src=http%3A%2F%2Fimg.mp.sohu.com%2Fupload%2F20170601%2Fd9ed13b4c839467791d719b8967b04b2_th.png',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602479246741&di=a77289efd90a24b433dc17f3cebb110c&imgtype=0&src=http%3A%2F%2Fwww.nutriciababy.com.cn%2Fd%2Ffile%2F96kaifa%2F201906081308%2F2019030587770007.png',
+      'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1106873249,1132735582&fm=26&gp=0.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602479246739&di=79ee3230e0e1ca59b1e83fed413f181c&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20181201%2F25e0bd97560e4ab7b144a2c8899afe83.gif',
+      'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=66348659,3312192893&fm=26&gp=0.jpg',
+      'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3140882653,2700593035&fm=26&gp=0.jpg',
+      'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3551488951,1342869485&fm=26&gp=0.jpg',
+      'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1394186084,4044813907&fm=15&gp=0.jpg',
+      'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2767673938,2867617806&fm=26&gp=0.jpg'
+    ]
+  }
+};
+
+export default waterfallService;
--- src/modules/waterfall/views/waterfall.vue
+++ src/modules/waterfall/views/waterfall.vue
@@ -2,41 +2,148 @@
   <hips-view class="waterfall" :header-height="50" :footer-height="48" header-fixed content-frozen>
     <!--导航栏-->
     <hips-nav-bar :back-button="{showLeftArrow: true}" title="瀑布流" @nav-bar-back-click="goBack()"/>
+    <hips-scroll ref="scroll" :data="[wrapperList]" :scroll-options="scrollOption" @pulling-down="refreshList"
+                 @pulling-up="loadMoreList">
+      <div class="waterfall-list">
+        <div class="fall-list" ref="fallList1">
+          <div class="fall-item" v-for="item in data1">
+            <img :src="item">
+          </div>
+        </div>
+        <div class="fall-list" ref="fallList2">
+          <div class="fall-item" v-for="item in data2">
+            <img :src="item">
+          </div>
+        </div>
+      </div>
+    </hips-scroll>
   </hips-view>
 </template>
 
 <script>
-  import {View, NavBar} from '@hips/vue-ui';
+  import {View, NavBar, Scroll} from '@hips/vue-ui';
+  import waterfallService from '../api';
 
   export default {
     name: "waterfall",
     components: {
       [View.name]: View,
-      [NavBar.name]: NavBar
+      [NavBar.name]: NavBar,
+      [Scroll.name]: Scroll
     },
     data() {
       return {
-        tabsActive: 0
+        //滚动加载的配置
+        scrollOption: {
+          pullDownRefresh: {
+            threshold: 60, // 触发 pullingDown 的距离
+            stop: 40, // pullingDown 正在刷新 hold 时的距离
+            txt: "刷新成功"
+          },
+          pullUpLoad: {
+            txt: {
+              more: "加载更多",
+              noMore: "-----我是有底线的-----"
+            }
+          }
+        },
+        page: 0,
+        data1: [],
+        data2: [],
       }
+    },
+    mounted() {
+      this.refreshList();
     },
     methods: {
       // 返回功能
       goBack() {
         this.$router.back();
       },
+
+      // 下拉刷新
+      refreshList() {
+        this.page = 0;
+        this.data1 = [];
+        this.data2 = [];
+        this.loadMoreList();
+      },
+
+      // 获取图片数据
+      async loadMoreList() {
+        this.throttle(async () => {
+          let res = await waterfallService.getImageList();
+          this.page++;
+          this.doSort(0, res);
+          if (this.page >= 5) {
+            this.$nextTick(() => {
+              this.$refs.scroll.forceUpdate();
+            })
+          }
+        })
+      },
+
+      // 图片排序
+      doSort(index, list) {
+        if (index < list.length) {
+          if (this.$refs.fallList1.clientHeight <= this.$refs.fallList2.clientHeight) {
+            this.data1.push(list[index]);
+          } else {
+            this.data2.push(list[index]);
+          }
+          let that = this;
+          this.$nextTick(() => {
+            setTimeout(() => {
+              that.doSort(index + 1, list);
+            }, 400);
+          })
+        }
+      },
+
+      // 函数节流
+      throttle(method, context) {
+        clearTimeout(method.tId);
+        method.tId = setTimeout(function () {
+          method.call(context);
+        }, 500)
+      }
+    },
+    computed: {
+      wrapperList() {
+        return [...this.data1, ...this.data2] // 请注意，在computed里面 包裹下list， 只有在特殊情况下，这么使用
+      }
     }
   }
 </script>
 
 <style scoped lang="stylus">
-  >>> .hips-nav-bar--default {
-    background: #1677ff;
-    .hips-nav-bar__buttons {
-      color #fff;
-      font-size 14px;
+  .waterfall {
+    background #f7f7f7;
+    >>> .hips-nav-bar--default {
+      background: #1677ff;
+      .hips-nav-bar__buttons {
+        color #fff;
+        font-size 14px;
+      }
+      .hips-nav-bar__title {
+        color #fff;
+      }
     }
-    .hips-nav-bar__title {
-      color #fff;
+
+    .waterfall-list {
+      position relative;
+      .fall-list {
+        width 50%;
+        float: left;
+        .fall-item {
+          width: 96%;
+          margin: 5px auto;
+          background-color: #fff
+          img {
+            width 100%;
+          }
+        }
+      }
     }
   }
 </style>
